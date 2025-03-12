d463c55cc1fbee19af9aec2c24de1ce75f0636e1
Neoathenian <42041261+Neoathenian@users.noreply.github.com>
2025-03-12 11:58:18 +0000

    Made it so that pandoc is in same folder

--- open_md.bat
+++ open_md.bat
@@ -6,11 +6,14 @@ cd "%~p1"
 REM Define the temp directory and the temp HTML file path
 set "TEMP_HTML=%TEMP%\%~n1.html"
 
+REM Define the path to Pandoc (assuming it's in the same directory as this .bat file)
+set "PANDOC=%~dp0pandoc.exe"
+
 REM Convert the Markdown file to HTML in the temp folder
-"C:\Program Files\Pandoc\pandoc.exe" "%~f1" -o "%TEMP_HTML%" --css="C:\scripts\style.css"
+"%PANDOC%" "%~f1" -o "%TEMP_HTML%" --css="C:\scripts\style.css"
 
 REM Convert to PDF (Optional) - still saving to the same directory
-"C:\Program Files\Pandoc\pandoc.exe" "%~f1" -o "%~dpn1.pdf"
+"%PANDOC%" "%~f1" -o "%~dpn1.pdf"
 
 REM Open the converted HTML file in Chrome
 start chrome "%TEMP_HTML%"
