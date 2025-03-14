acbc7d9fb76743757fe2c63f877b7fec9e1eb6c7
Neoathenian <42041261+Neoathenian@users.noreply.github.com>
2025-03-14 11:24:46 +0000

    Opens with default browser now

--- open_md.bat
+++ open_md.bat
@@ -16,4 +16,5 @@ REM Convert to PDF (Optional) - still saving to the same directory
 "%PANDOC%" "%~f1" -o "%~dpn1.pdf"
 
 REM Open the converted HTML file in Chrome
-start chrome "%TEMP_HTML%"
+start "" "%TEMP_HTML%"
+
